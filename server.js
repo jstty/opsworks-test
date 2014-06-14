87fdedf29b24012bb0ec275055349d9ba70f7173
Joe <joe@jstty.com>
2014-06-14 13:36:58 -0700

    added opsworks config added connect to RDS

--- server.js
+++ server.js
@@ -2,14 +2,35 @@
  * Created by josephsutton on 6/13/14.
  */
 
+var opsworksConfig  = require('shared/config/opsworks.js');
+
 var express = require('express');
 var app     = express();
-
 var port = 80;
+
+// http://docs.aws.amazon.com/opsworks/latest/userguide/workingapps-connectdb.html
+if( opsworksConfig &&
+    opsworksConfig.db &&
+    opsworksConfig.db.adapter == "mysql") {
+
+    opsworksConfig.db.user = opsworksConfig.db.username;
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection(opsworksConfig.db);
+}
+
 
 // Routes
 app.get('/', function(req, res) {
-    res.send('Hello World:', process.env);
+    connection.connect();
+    connection.query('SELECT * from test_table', function(err, rows, fields) {
+        if (err) {
+            res.send('Error:', err);
+            return;
+        }
+
+        res.send('Data:', rows);
+    });
+    connection.end();
 });
 
 app.listen(port);
